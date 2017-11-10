61983a02fb8d90e7fd9985ff26c3c0543e8ab322
Li <hli3302@gmail.com>
2017-11-10 11:36:17 -0500

    first commit

---
+++ src/PdfParse.java
@@ -0,0 +1,38 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.pdf.PDFParser;
+import org.apache.tika.sax.BodyContentHandler;
+
+import org.xml.sax.SAXException;
+
+public class PdfParse {
+
+   public static void main(final String[] args) throws IOException,TikaException, SAXException {
+
+      BodyContentHandler handler = new BodyContentHandler();
+      Metadata metadata = new Metadata();
+      BufferedWriter bw = null;
+	  FileWriter fw = null;
+ 	  try {
+			fw = new FileWriter(args[1]);
+			bw = new BufferedWriter(fw);
+   	  } catch (IOException e) {
+    	    System.out.println("Please check your output folder "+e.getMessage());
+      }
+      FileInputStream inputstream = new FileInputStream(new File(args[0]));
+      ParseContext pcontext = new ParseContext();
+      PDFParser pdfparser = new PDFParser(); 
+      pdfparser.parse(inputstream, handler, metadata,pcontext);
+      bw.write(handler.toString());
+      bw.close();
+    
+   }
+}
